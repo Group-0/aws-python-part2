c1c65f20ba6a960cabf32917caad95b79ead7f49
Paula Letícia Gerónimo <paulagero182@gmail.com>
2022-09-27 16:14:19 -0500

    Updated file layout/tabbing

--- main.py
+++ main.py
@@ -15,38 +15,38 @@ amt_of_least_requested = 0
 # Functions to use
 #   Paula's Code: the function to calculate the requests made in 6 months
 def getSixMonths(file):
-    global amt_request_six_months
-    
-    Content = file.read()
-    line_list = Content.split("\n")
-    # startMonAndDay: "24/Oct/1994"
+  global amt_request_six_months
+  
+  Content = file.read()
+  line_list = Content.split("\n")
+  # startMonAndDay: "24/Oct/1994"
 
-    for line in line_list:
-        if line:
-            amt_request_six_months += 1
-            if "24/May/1995" in line:
-                return (amt_request_six_months - 1)
+  for line in line_list:
+    if line:
+      amt_request_six_months += 1
+      if "24/May/1995" in line:
+        return (amt_request_six_months - 1)
 
 #   Irish's Code: function to get the percent of requests for a particular request code starting with string num (i.e., 4xx, 3xx)
 def getRequestsPercent(file, num): 
-    global amt_request_total
-    # Append first digit of request code we're looking for (num) to the regex pattern
-    pattern = ".*\[([0-9]+/[a-zA-Z]+/[0-9]{4}):(.*) \-[0-9]{4}\] \"(.*)\" " + num + ".*"
-    amt_requests = 0
+  global amt_request_total
+  # Append first digit of request code we're looking for (num) to the regex pattern
+  pattern = ".*\[([0-9]+/[a-zA-Z]+/[0-9]{4}):(.*) \-[0-9]{4}\] \"(.*)\" " + num + ".*"
+  amt_requests = 0
 
-    # Loop through each line of file
-    for line in file:
-        # Read line of file one by one and temporarily store as one_line
-        one_line = file.readline()
+  # Loop through each line of file
+  for line in file:
+    # Read line of file one by one and temporarily store as one_line
+    one_line = file.readline()
 
-        # Use regex function re.search() to check if current line matches with regex pattern
-        if re.search(pattern, one_line):
-            # If it matches, increment amt_requests by 1
-            amt_requests += 1
-    
-    # Get percentage; divide amt_requests by amt_request_total
-    requests_percent = (amt_requests / amt_request_total) * 100
-    return round(requests_percent, 2)
+    # Use regex function re.search() to check if current line matches with regex pattern
+    if re.search(pattern, one_line):
+      # If it matches, increment amt_requests by 1
+      amt_requests += 1
+  
+  # Get percentage; divide amt_requests by amt_request_total
+  requests_percent = (amt_requests / amt_request_total) * 100
+  return round(requests_percent, 2)
 
 def findCountMonth():
   global month_count
@@ -67,25 +67,22 @@ def getFileCount():
     pieces = re.split(
       ".*\[([0-9]+/[a-zA-Z]+/[0-9]{4}):(.*) \-[0-9]{4}\] \"(?:GET )(.*)(?: HTTP/1.0)\" .*",
       line)
-      # Let's further say that we can get the filename part at the 4th list element
-
+      
     if len(pieces) > 3:
       filename = pieces[3]
-      
       if filename in file_count:
         file_count[filename] += 1
       else:
-        
         file_count[filename] = 1
 
 # Claire's Code: Fetching the log file from the Apache server
 URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 
 if os.path.exists('http_access_log.txt'):  
-    log_file = 'http_access_log.txt'
+  log_file = 'http_access_log.txt'
 else:
-    print("Loading Log File...")
-    log_file, headers = urlretrieve(URL_PATH, 'http_access_log.txt', lambda x,y,z: print('.', end='', flush=True) if x % 100 == 0 else False)
+  print("Loading Log File...")
+  log_file, headers = urlretrieve(URL_PATH, 'http_access_log.txt', lambda x,y,z: print('.', end='', flush=True) if x % 100 == 0 else False)
 
 print("\nDone Loading.")
 os.system('clear')
@@ -97,8 +94,8 @@ amt_request_six_months = getSixMonths(file)
 # Irish's Code: Calculating total amount of requests
 # opens & reads log file as filehead file
 with open(log_file, "r") as file:
-    # stores each line in list & counts list length
-    amt_request_total= len(file.readlines())
+  # stores each line in list & counts list length
+  amt_request_total= len(file.readlines())
 
 # Roxanna's Code: Outputting the requested amounts
 print("Done analyzing log files.")
@@ -124,13 +121,13 @@ print("-"*20)
 # Irish's Code: Percentage of requests that were unsuccessful (4xx request codes)
 # Open and read file
 with open(log_file, "r") as file:
-    # Call function with correct parameters and print results
-    print("Percent of Unsuccessful Requests: ", getRequestsPercent(file, "4"), "%")
+  # Call function with correct parameters and print results
+  print("Percent of Unsuccessful Requests: ", getRequestsPercent(file, "4"), "%")
 
 # Juan's Code: Percentage of requests that were redirected elsewhere (any 3xx request codes)
 # Open and read file
 with open(log_file, "r") as file:
-    # Call function with correct parameters and print results
-    print("Percent of Redirected Requests: ", getRequestsPercent(file, "3"), "%")
+  # Call function with correct parameters and print results
+  print("Percent of Redirected Requests: ", getRequestsPercent(file, "3"), "%")
     
 print("-"*20)
